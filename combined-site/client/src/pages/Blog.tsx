e2bd5a6b4d93fc1c00c14da5dc073cbe8fad3026
Praveen4278 <gpraveenraj03@gmail.com>
2026-08-14 09:05:09 +0530

    Reduce mobile Success Stories heading font size to prevent mid-line wrap

--- combined-site/client/src/pages/Blog.tsx
+++ combined-site/client/src/pages/Blog.tsx
@@ -1916,7 +1916,7 @@ const BlogDesktop = () => {
             </div>
             {/* Heading */}
             <div className="flex justify-center px-[8px]">
-              <span className="text-center text-[34px] leading-[38px] text-[#111111]" style={{ fontFamily: "SF Pro Display", fontWeight: 400 }}>
+              <span className="text-center text-[28px] leading-[34px] text-[#111111]" style={{ fontFamily: "SF Pro Display", fontWeight: 400 }}>
                 Real Results. Real Impact.<br />Our Success Stories
               </span>
             </div>
